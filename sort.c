2105aaef4b16c0a97f18ae33cbf246845f855ce7
ktajimagnct <ktajima@gifu-nct.ac.jp>
2020-06-08 13:31:32 +0900

    Create sort.c
    最初のバージョンです。

---
+++ sort.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+void printArray(int *array);
+int sortArray(int *array);
+
+int main(int argc, char *argv[]){
+	printf("git test.\n");
+	sortArray();
+	printArray();
+}
+
+void printArray(int *array){
+//TODO 田島の担当部分です。担当者以外は触らないでね。
+}
+
+int sortArray(int *array){
+//TODO　丹羽がコードをここに作る
+}
